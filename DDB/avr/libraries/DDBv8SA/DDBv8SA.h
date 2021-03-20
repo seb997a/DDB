7a649835c214bb4127651b379cdeb40919113c15
seb997a <78752958+seb997a@users.noreply.github.com>
2021-03-20 12:49:19 -0400

    1.0.3

--- DDB/avr/libraries/DDBv8SA/DDBv8SA.h
+++ DDB/avr/libraries/DDBv8SA/DDBv8SA.h
@@ -16,6 +16,19 @@
 #define WDTps512K   1<<WDP3|0<<WDP2|0<<WDP1|0<<WDP0    // ~4s
 #define WDTps1024K  1<<WDP3|0<<WDP2|0<<WDP1|1<<WDP0    // ~8s
 
+uint8_t segments[] = {            //ADC! MSBFIRST: ABCDEFGx
+  0b11111100,//0
+  0b01100000,//1
+  0b11011010,//2
+  0b11110010,//3
+  0b01100110,//4
+  0b10110110,//5
+  0b10111110,//6
+  0b11100000,//7
+  0b11111110,//8
+  0b11110110,//9
+};
+
 uint8_t characterMap[] {
   0x3F,
   0x06,
