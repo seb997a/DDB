78fba0a136c4e14a71e5b280624f9ee841b8422e
seb997a <78752958+seb997a@users.noreply.github.com>
2021-04-17 18:07:52 -0400

    library update

--- DDB/avr/libraries/DDBv8SA/examples/softwareSerialExample/softwareSerialExample.ino
+++ DDB/avr/libraries/DDBv8SA/examples/softwareSerialExample/softwareSerialExample.ino
@@ -1,17 +1,17 @@
 /*
-The sketch demonstrates how to use the serial monitor on the DBDV8. 
-To upload, click upload (cmd u) and wait for the terminal window to say uploading.
-When it says uploading, connect a MINI-USB to the "MINI-USB-Flash" USB port.
-Once the sketch has been uploaded, connect the USB to the "MINI-USB-FLASH" port.
-Once connected, select the correct COM port in the IDE and wait until the onboard LED illuminates.
-Once illuminated, open the serial monitor and make sure the baud rate is set to "38400".
-To test the sketch, type something into the serial monitor, and it should print it back out.
+  The sketch demonstrates how to use the serial monitor on the DBDV8.
+  To upload, click upload (cmd u) and wait for the terminal window to say uploading.
+  When it says uploading, connect a MINI-USB to the "MINI-USB-Flash" USB port.
+  Once the sketch has been uploaded, connect the USB to the "MINI-USB-FLASH" port.
+  Once connected, select the correct COM port in the IDE and wait until the onboard LED illuminates.
+  Once illuminated, open the serial monitor and make sure the baud rate is set to 38400.
+  To test that the serial monitor is working, type anything into the serial monitor and then "test" should be returned.
 */
 
 #include<DDBv8SA.h>
 #include <SoftwareSerial.h>
 
-char message;
+volatile char message;
 
 SoftwareSerial mySerial(RXPIN, TXPIN); // RX, TX
 
@@ -20,9 +20,9 @@ void setup() {
 }
 
 void loop() {
-  onBoardLED(1);                      //Turns LED on informing the user that the sketch is running.
-  while (mySerial.available()) {
-    message = mySerial.read();
-    mySerial.write(message);
+  if (mySerial.available()) {
+    mySerial.println("test");
+    while (mySerial.available())
+      mySerial.read();
   }
 }
